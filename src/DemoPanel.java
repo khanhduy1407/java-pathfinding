a6fb187c4a26433a8423ce22c560cc6997dc9a42
NKDuy <kn145660@gmail.com>
2023-02-02 22:26:39 +0700

    chore: Creating Node class

--- src/DemoPanel.java
+++ src/DemoPanel.java
@@ -6,9 +6,12 @@ public class DemoPanel extends JPanel {
     // SCREEN SETTINGS
     final int maxCol = 15;
     final int maxRow = 10;
-    final int nodeSize = 70;
+    final int nodeSize = 60;
     final int screenWidth = nodeSize * maxCol;
     final int screenHeight = nodeSize * maxRow;
+
+    // NODE
+    Node[][] node = new Node[maxCol][maxRow];
 
     public DemoPanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
---
+++ src/Node.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Node extends JButton implements ActionListener {
+
+    Node parent;
+    int col;
+    int row;
+    int gCost;
+    int hCost;
+    int fCost;
+    boolean start;
+    boolean goal;
+    boolean solid;
+    boolean open;
+    boolean checked;
+
+    public Node(int col, int row) {
+        this.col = col;
+        this.row = row;
+
+        setBackground(Color.white);
+        setForeground(Color.black);
+        addActionListener(this);
+    }
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setBackground(Color.orange);
+    }
+}
