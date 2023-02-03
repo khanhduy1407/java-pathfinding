7034d2d2a1fd5d34a4f88af887c84636389441f2
NKDuy <kn145660@gmail.com>
2023-02-03 07:33:30 +0700

    chore: Placing nodes on the panel

--- src/DemoPanel.java
+++ src/DemoPanel.java
@@ -16,6 +16,21 @@ public class DemoPanel extends JPanel {
     public DemoPanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
-        this.setLayout(new GridLayout(maxCol, maxRow));
+        this.setLayout(new GridLayout(maxRow, maxCol));
+
+        // PLACE NODES
+        int col = 0;
+        int row  = 0;
+
+        while (col < maxCol && row < maxRow) {
+            node[col][row] = new Node(col, row);
+            this.add(node[col][row]);
+
+            col++;
+            if (col == maxCol) {
+                col = 0;
+                row++;
+            }
+        }
     }
 }
